da85aac825d6a18abc927e31dfdb073b5de5eead
Rahuljr10 <89572278+Rahuljr10@users.noreply.github.com>
2021-10-29 02:04:28 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     azurerm = {
-      version = "=1.0.10"
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
     }
   }
 }
-
 provider "azurerm" {
   features {}
 }
