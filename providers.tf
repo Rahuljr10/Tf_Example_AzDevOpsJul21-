fc6a9b4695486dc04a108cfc3d16ebb3eb3848fa
Rahuljr10 <89572278+Rahuljr10@users.noreply.github.com>
2021-10-29 02:25:04 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = "1.0.10"
     }
   }
 }
@@ -10,11 +10,3 @@ provider "azurerm" {
   features {}
 }
 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tf-rg"
-    storage_account_name = "azexampleseptdemo"
-    container_name       = "tfstatedemo"
-    key                  = "terraform.tfstate"
-  }
-}
